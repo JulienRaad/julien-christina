3277764cafeb9d4327ecd4c204a5d427c1f2be86
JulienRaad <Julien.praad@gmail.com>
2025-06-14 21:25:17 +0200

    Update search.js

--- public/search.js
+++ public/search.js
@@ -3,47 +3,54 @@ const list = document.getElementById("suggestions");
 const searchBox = document.getElementById("guest-search");
 
 fetch("guests.json")
-  .then((resp) => resp.json())
-  .then((data) => {
-    allGuests = data
-    renderSuggestions(allGuests, "name");
-  })
-  .catch((err) => {
-    console.error("Could not load guests.json:", err);
-  });
+	.then((resp) => resp.json())
+	.then((data) => {
+		allGuests = data
+		renderSuggestions(allGuests, "name");
+	})
+	.catch((err) => {
+		console.error("Could not load guests.json:", err);
+	});
 
 searchBox.addEventListener("input", () => {
-  const query = searchBox.value.trim().toLowerCase();
-  if (!query) {
-    renderSuggestions(allGuests, "name");
-    return;
-  }
+	const query = searchBox.value.trim().toLowerCase();
+	if (!query) {
+		renderSuggestions(allGuests, "name");
+		return;
+	}
+
   let groupBy = "name";
-  let filteredGuests = []
-  filteredGuests = allGuests.filter((guest) => guest.name.toLowerCase().startsWith(query));
-  if (filteredGuests.length === 0) {
-    filteredGuests = allGuests.filter((guest) => guest.table === query)
+	let filteredGuests = []
+	if (query.startsWith("id=")) {
+		const id = str.split("=")[1];
     groupBy = "table"
-  }
-  renderSuggestions(filteredGuests, groupBy);
+		filteredGuests = allGuests.filter((guest) => guest.id === id)
+	} else {
+		filteredGuests = allGuests.filter((guest) => guest.name.toLowerCase().startsWith(query));
+		if (filteredGuests.length === 0) {
+			filteredGuests = allGuests.filter((guest) => guest.table === query)
+			groupBy = "table"
+		}
+	}
+	renderSuggestions(filteredGuests, groupBy);
 });
 
 function renderSuggestions(matchedGuests, groupBy) {
-  list.innerHTML = "";
-  const sortedMatchedGuests = matchedGuests.sort((previousGuest, currentGuest) => previousGuest.name.localeCompare(currentGuest.name));
-  let currentGroup = null;
+	list.innerHTML = "";
+	const sortedMatchedGuests = matchedGuests.sort((previousGuest, currentGuest) => previousGuest.name.localeCompare(currentGuest.name));
+	let currentGroup = null;
 
-  sortedMatchedGuests.forEach((guest) => {
-    const groupKey = groupBy === "table" ? guest.table : guest.name.charAt(0).toUpperCase();
-    if (groupKey !== currentGroup) {
-      currentGroup = groupKey;
-      const heading = document.createElement("li");
-      heading.textContent = groupBy === "table" ? `Table ${groupKey}` : groupKey;
-      heading.classList.add("group-letter");
-      list.appendChild(heading);
-    }
-    const li = document.createElement("li");
-    li.innerHTML = groupBy === "table" ? `<span class="name">${guest.name}</span>`: `<span class="name">${guest.name}</span> <span class="table">${guest.table}</span>`;
-    list.appendChild(li);
-  });
+	sortedMatchedGuests.forEach((guest) => {
+		const groupKey = groupBy === "table" ? guest.table : guest.name.charAt(0).toUpperCase();
+		if (groupKey !== currentGroup) {
+			currentGroup = groupKey;
+			const heading = document.createElement("li");
+			heading.textContent = groupBy === "table" ? `Table ${groupKey}` : groupKey;
+			heading.classList.add("group-letter");
+			list.appendChild(heading);
+		}
+		const li = document.createElement("li");
+		li.innerHTML = groupBy === "table" ? `<span class="name">${guest.name}</span>` : `<span class="name">${guest.name}</span> <span class="table">${guest.table}</span>`;
+		list.appendChild(li);
+	});
 }
